08633426a3107c6ae56027a5c694340f50102a6f
sonibk <67633694+sonibk@users.noreply.github.com>
2023-02-13 13:18:50 +0300

    Update bkamau_a1.py

--- bkamau_a1.py
+++ bkamau_a1.py
@@ -11,34 +11,35 @@ age=[int(i) for i in age]
 height=[180,170, float("Nan"), 175,190,190,195,200,205,210]
 
 weight=[70,65,"80",75,90,90,95,100,105,110]
-##A list comprehension to convert "80" from a string to an interger
+
+##A list comprehension to convert all the elements in the weight list into integers eg. "80"
 weight=[int(i) for i in weight]
 
 ##creating a dictionary
 df={"id":id,"age":age,"height":height,"weight":weight}
 
-##creating a dataframe
+##creating a dataframe from the dictionary using pandas
 df=pd.DataFrame(df)
 
-##checking for missing values
+##checking for missing values in the dataframe
 df.isna().sum()
 
-##dropping missing values
+##dropping any missing values detected
 df.dropna(inplace=True)
 
 ## a function that returns the mean, median and standard deviation of weight column
-def summary_statistics(data, column):
+def summary_statistics(data, weight):
     #calculation the mean
-        mean_weight=sum(column)/len(column)
+        mean_weight=sum(weight)/len(weight)
     #calculating the median
-        median_weight=sorted(column)
+        median_weight=sorted(weight)
         n=len(median_weight)
         if n%2 == 0:
             median = (median_weight[n // 2 - 1] + median_weight[n // 2]) / 2
         else:
              median = median_weight[n // 2]
      # Calculating the standard deviation
-        variance = sum((i - mean_weight) ** 2 for i in column) / len(column)
+        variance = sum((i - mean_weight) ** 2 for i in weight) / len(weight)
         std_dev = variance ** 0.5
         return(mean_weight,median,std_dev)
     
